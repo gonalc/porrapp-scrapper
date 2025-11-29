e71db4050731c0c23140dc7baa008880fc378435
Gonzalo <gonalc8@gmail.com>
2025-11-29 18:57:15 +0100

    handle game finished stats

--- src/playground.ts
+++
@@ -1,7 +0,0 @@
-import { UserStatsService } from "./services/user-stats";
-
-const gameCode = "01_0101_20251129_855_174"
-
-const statsService = new UserStatsService();
-
-await statsService.handleFinishedGame(gameCode);
